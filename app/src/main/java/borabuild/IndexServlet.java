eae994b2971f8668fd62d44e1d66e09f74c3a9df
Rafael8000 <rafael.pomari@etec.sp.gov.br>
2023-05-03 17:07:27 +0000

    pastel de maconha

--- app/src/main/java/borabuild/IndexServlet.java
+++ app/src/main/java/borabuild/IndexServlet.java
@@ -1,16 +1,18 @@
 package borabuild;
 
+ import java.io.IOException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 @WebServlet(name="IndexServlet", urlPatterns = "/")
 public class IndexServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException  {
-        response.getWriter().println("Oi servlet");
-        response.getWriter().close();
+       // response.getWriter().println("Oi servlet");
+       // response.getWriter().close();
+       request.getRequestDispatcher("home.jsp").forward(request, response);
     }
     
 }
---
+++ app/src/main/webapp/home.jsp
@@ -0,0 +1,11 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>vinland saga</title>
+</head>
+<body>
+    <h1> escute com atenção thorfinn </h1>
+</body>
+</html>
